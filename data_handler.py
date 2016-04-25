5dfc6fc01de8968bb074fe21a8f285d5b228a273
Jianing Song <eric.songjn@icloud.com>
2016-04-24 22:55:54 -0500

    1st commit from ide

--- data_handler.py
+++ data_handler.py
@@ -1,3 +1,10 @@
 import pandas as pd
-
-
+import pandas_datareader as pd_datareader
+# current data source:
+# 1. Wind terminal with python api. (Chinese)
+# 2. Pandas DataReader.( Yahoo & Google )
+try:
+    from WindPy import *
+except ImportError:
+    print "Program needs to run with Wind Terminal or Wind Python API."
+    pass
